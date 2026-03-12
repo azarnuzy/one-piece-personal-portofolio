0e4974c9f1935999f5a72a108fff2ae3fedafd97
Nathaniel John Tampus <nat@nize.ph>
2026-03-13 06:48:37 +0800

    chore: readme note for temporary @vitejs/plugin-react workaround

--- src/components/_DELETE_ME_intro_page.tsx
+++ src/components/_DELETE_ME_intro_page.tsx
@@ -359,7 +359,7 @@ const DATA_BADGES: TechBadge[] = [
 const PLATFORM_BADGES: TechBadge[] = [
   {
     alt: "Vite version",
-    href: "https://vite.dev/blog/announcing-vite8-beta",
+    href: "https://vite.dev",
     src: "https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fraw.githubusercontent.com%2Fmugnavo%2Ftanstarter%2Fmain%2Fpackage.json&query=%24.devDependencies.vite&label=vite&style=flat-square",
   },
   {
