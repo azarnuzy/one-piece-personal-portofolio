2998d21ec78f105b869e7668a71d8942cc560bcd
Nathaniel John Tampus <nat@nize.ph>
2026-03-30 13:05:52 +0800

    chore: ensure authClient is client-only

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,9 +1,18 @@
+import { createClientOnlyFn } from "@tanstack/react-start";
 import { createAuthClient } from "better-auth/react";
 
 import { env } from "@/env/client";
 
-const authClient = createAuthClient({
-  baseURL: env.VITE_BASE_URL,
-});
+/**
+ * Our better-auth server instance lives in the TanStack Start server,
+ * so authClient is only used on the client/browser (e.g. event handlers, effects, etc).
+ *
+ * For server/SSR operations, prefer `auth.api` instead, and wrap in a serverFn if needed.
+ */
+const getAuthClient = createClientOnlyFn(() =>
+  createAuthClient({
+    baseURL: env.VITE_BASE_URL,
+  }),
+);
 
-export default authClient;
+export const authClient = getAuthClient();
