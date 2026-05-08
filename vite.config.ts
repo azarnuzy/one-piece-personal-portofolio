e9de2d19932b0f12cbc754cc07c9cdd5d43620df
azarnuzy <azarnuzy@gmail.com>
2026-05-08 11:36:33 +0700

    chore: fix configuration neltify

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
@@ -18,8 +18,7 @@ export default defineConfig({
     svgr(),
     devtools(),
     tanstackStart(),
-    // https://tanstack.com/start/latest/docs/framework/react/guide/hosting
-    nitro(),
+    netlify(),
     viteReact(),
     // https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md#react-compiler
     babel({
