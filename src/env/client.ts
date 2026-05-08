7f368f490570bba8db901cc68ca62231db9660a8
azarnuzy <azarnuzy@gmail.com>
2026-05-08 10:51:34 +0700

    fix: skip env validation

--- src/env/client.ts
+++ src/env/client.ts
@@ -2,6 +2,7 @@ import { createEnv } from "@t3-oss/env-core";
 import * as z from "zod";
 
 export const env = createEnv({
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   clientPrefix: "VITE_",
   client: {
     VITE_BASE_URL: z.url().default("http://localhost:3000"),
